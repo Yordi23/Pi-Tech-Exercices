ee4f6f17aa1513b45d54cc66215d190a986bdd96
Yordi Ogando <yordiogando@gmail.com>
2019-11-07 18:39:11 -0400

    Hundreds done

--- Pi-Tech Exercises/Ejercicio4/Ejercicio4.cs
+++ Pi-Tech Exercises/Ejercicio4/Ejercicio4.cs
@@ -14,17 +14,39 @@ namespace Ejercicio4
                 "once", "doce", "trece", "catorce", "quince", "diezciseis", "diecisiete", "dieciocho", "diecinueve" };
             //string[] specials = {"once", "doce","trece","catorce", "quince","diezciseis", "diecisiete", "dieciocho", "diecinueve"};
             string[] tens ={"veinte", "treinta","cuarenta","cincuenta","sesenta", "setenta", "ochenta", "noventa"};
+            string[] hundreds = {"quinientos",null,"setecientos",null,"novecientos"};
 
             if (num < 20) output = units[num];
 
-            else if(num < 100)
+            else if (num < 100)
             {
-                int dec = num / 10;
-                int u = num % 10;
+                int ten = num / 10;
+                int unit = num % 10;
+                
 
-                if (u == 0) return tens[dec-2];
+                if (unit == 0) output = tens[ten - 2];
 
-                output = string.Format("{0} y {1}", Convert(((num / 10) * 10)), (u == 1 ? "uno" : Convert(num % 10)));
+                else output = string.Format("{0} y {1}", Convert(((num / 10) * 10)), (unit == 1 ? "uno" : Convert(num % 10)));
+            }
+            
+            else if (num == 100) output = "cien";
+
+            else if (num < 200) output = "ciento " + Convert(num - 100);
+
+            else if (num < 1000)
+            {
+                int ten = (num / 10) % 10;
+                int unit = num % 10;
+                int hundred = (num / 100) % 10;
+
+                if (ten == 0 && unit == 0)
+                {
+                    if (hundred == 5 || hundred == 7 || hundred == 9) output = hundreds[hundred - 5];
+
+                    else output = Convert((num / 100)) + "cientos";
+                }
+
+                else output = string.Format("{0} {1}", Convert((num / 100) * 100), Convert(num % 100));
             }
 
 
@@ -34,8 +56,8 @@ namespace Ejercicio4
 
         public int[] GenerateArray()
         {
-            int[] arr = new int[100];
-            for (int i = 0; i < 100; i++)
+            int[] arr = new int[1000];
+            for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = i;
             }
