dc957183f4bb1f642c01a4d15f5f7cfd72b8aa56
Yordi Ogando <yordiogando@gmail.com>
2019-11-07 01:33:23 -0400

    Exercise2 Done

--- Pi-Tech Exercises/Ejercicio 2/Ejercicio2.cs
+++ Pi-Tech Exercises/Ejercicio 2/Ejercicio2.cs
@@ -7,66 +7,40 @@ using System.Text;
 namespace Ejercicio_2
 {
     class Ejercicio2
-    {
-        
+    {   
         public void GenerateExercise(string path)
         {
             string instruction = "Selecciona el resultado de la siguiente suma.";
-
             string[] problem = new string[2];
             string[] options = new string[4];
-
             int resutl = 0;
-
             Random rdn = new Random();
 
 
+            //Generamos de manera aleatoria dos números para realizar la operación de suma.
             for (int i = 0; i < problem.Length; i++)
             {
                 //Debido a que no se especifica en el ejercicio, elegí arbitrariamente que 
                 //el número máximo para realizar las operaciones de suma sería 100,000.
                 int num = rdn.Next(100001);
                 resutl += num;
-                problem[i] = ParseNum(num);
+                problem[i] = String.Format("{0:n0}", num);
             }
 
-            options[rdn.Next(3)] = ParseNum(resutl);
+
+            options[rdn.Next(4)] = String.Format("{0:n0}", resutl);
             options = GenerateOptions(options);
 
 
             EjercicioSuma ejercicio = new EjercicioSuma(instruction,problem,options,resutl);
-
             SerializeAndExport(path, ejercicio);
         }
+        //Nota: A pesar de que en el ejemplo se muestra que dentro del archivo Json el campo de 
+        //resultado es igual a 0, asumí que este campo corresponde al resultado de realizar la 
+        //operación de suma.
 
 
-        private string ParseNum(int num)
-        {
-            string strNum = Convert.ToString(num), output = "";
-            List<char> temp = new List<char>();
-            int count = 0;
-
-            for (int i = strNum.Length-1; i > 0; i--)
-            {
-                if (count == 3)
-                {
-                    temp.Add(',');
-                    count = 0;
-                }
-
-                else
-                {
-                    temp.Add(strNum[i]);
-                    count++;
-                }
-            }
-            for (int i = 0; i < temp.Count; i++)
-                output += temp[i];
-
-            return output;
-        }
-
-        //Se crean opciones aleatorias.
+        //Se generan opciones aleatorias.
         private string[] GenerateOptions (string[] arr)
         {
             Random rdn = new Random();
@@ -74,11 +48,13 @@ namespace Ejercicio_2
             for (int i = 0; i < arr.Length; i++)
             {
                 //El resultado posible más grande es 200,000 (100,000 + 100,000)
-                if (arr[i] == null) rdn.Next(200001);
+                if (arr[i] == null) arr[i] = String.Format("{0:n0}", rdn.Next(200001));
             }
 
             return arr;
         }
+
+
 
         //Se convierte el objeto a formato Json y se exporta.
         private void SerializeAndExport(string path, EjercicioSuma ejercicio)
